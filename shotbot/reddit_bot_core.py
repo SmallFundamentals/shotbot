4068f788ce8234a3008c53a25e1d0e40891cebee
Blake Yu <smallfundamentals@gmail.com>
2016-02-16 21:54:00 -0800

    Change tabs to spaces

--- shotbot/reddit_bot_core.py
+++ shotbot/reddit_bot_core.py
@@ -4,15 +4,15 @@ import praw
 
 class RedditBotCore(object):
 
-	USER_AGENT = "Web:NBAShotcharts:0.1.0 (by /u/smallfundamentals)"
-	SUBREDDIT = "nba"
-	TEST_SUBREDDIT = "anarmadillo"
+    USER_AGENT = "Web:NBAShotcharts:0.1.0 (by /u/smallfundamentals)"
+    SUBREDDIT = "nba"
+    TEST_SUBREDDIT = "anarmadillo"
 
-	def __init__(self):
-		self.r = praw.Reddit(self.USER_AGENT)
-		self.r.login(disable_warning=True)
-		self.subreddit = self.r.get_subreddit(self.TEST_SUBREDDIT)
+    def __init__(self):
+        self.r = praw.Reddit(self.USER_AGENT)
+        self.r.login(disable_warning=True)
+        self.subreddit = self.r.get_subreddit(self.TEST_SUBREDDIT)
 
-	def get_query_from_comment(self, comment, pattern):
-		match = re.search(pattern, comment)
-		return match
+    def get_query_from_comment(self, comment, pattern):
+        match = re.search(pattern, comment)
+        return match
