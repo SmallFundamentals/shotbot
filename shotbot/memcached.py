b2892dfe3e58684fa21674ca1b07868840683715
Blake Yu <smallfundamentals@gmail.com>
2016-02-20 21:25:07 -0800

    Ready for extensive testing

--- shotbot/memcached.py
+++ shotbot/memcached.py
@@ -4,10 +4,11 @@ import memcache
 
 memcached_client = memcache.Client(['127.0.0.1:11211'], debug=0)
 
-def generate_key(player_id):
+def generate_key_for_player_id_chart_kind(player_id, chart_kind):
 	if player_id:
-		key = "[{date}]{player_id}".format(
+		key = "[{date}]-{player_id}-{chart_kind}".format(
 			date=date.today().isoformat(),
-			player_id=player_id
+			player_id=player_id,
+			chart_kind=chart_kind
 		)
 		return key
