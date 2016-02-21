be006539bb04e7edea69a9dc9e14f539aae753e7
Blake Yu <smallfundamentals@gmail.com>
2016-02-20 20:10:08 -0800

    Satisfactory hex implementation

--- shotbot/__main__.py
+++ shotbot/__main__.py
@@ -12,6 +12,8 @@ def main(args):
         shotbot.start()
     elif len(args) == 2:
         shotbot.generate(args[1])
+    elif len(args) == 3:
+        shotbot.generate(args[1], args[2])
 
 if __name__ == "__main__":
     main(sys.argv)
